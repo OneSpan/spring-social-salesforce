78f9c0c4e93bed3506c33033d0633b3ec83aad7b
Antoine Beauvais-Lacasse <antoine_lacasse@silanis.com>
2015-03-31 14:08:59 -0400

    Use endpoint instead of state to allow redirection from custom salesforce portals. More flexible error handling.

--- src/main/java/org/springframework/social/salesforce/connect/SalesforceOAuth2Template.java
+++ src/main/java/org/springframework/social/salesforce/connect/SalesforceOAuth2Template.java
@@ -6,16 +6,15 @@ import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.social.oauth2.OAuth2Template;
 import org.springframework.util.StringUtils;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 /**
  * Salesforce OAuth2Template implementation.
@@ -27,8 +26,8 @@ import java.util.regex.Pattern;
 public class SalesforceOAuth2Template extends OAuth2Template {
 
     private static final Pattern TLD_REGEX = Pattern.compile(".*?([^.]+\\.[^.]+)");
-    private static final List<String> SALESFORCE_TLDS = Arrays.asList("force.com", "salesforce.com");
 
+    private static final List<String> VALID_SALESFORCE_DOMAINS = Arrays.asList("force.com", "salesforce.com");
 
     static final String AUTHORIZE_PATH = "services/oauth2/authorize";
     static final String TOKEN_PATH = "services/oauth2/token";
@@ -48,35 +47,35 @@ public class SalesforceOAuth2Template extends OAuth2Template {
     @Override
     public String buildAuthorizeUrl(GrantType grantType, OAuth2Parameters parameters) {
 
-        String endpointUrlParam = parameters.getState();
+        String endpointUrlParam = parameters.getFirst("endpoint");
+
+        /* Endpoint parameter should be removed before building the Authorize URL:
+           We are only using it for use with custom SF domains. */
+        parameters.remove("endpoint");
+
+        final String baseAuthorizeURL = super.buildAuthorizeUrl(grantType, parameters);
+
         if (StringUtils.isEmpty(endpointUrlParam)) {
-            return super.buildAuthorizeUrl(grantType, parameters);
+            return baseAuthorizeURL;
         } else {
 
             boolean isValidSalesforceHostname;
             String endpointUrl;
             try {
-                endpointUrl = URLDecoder.decode(endpointUrlParam, StandardCharsets.UTF_8.name());
+                endpointUrl = URLDecoder.decode(endpointUrlParam, UTF_8.name());
                 final URI uri = new URI(endpointUrl);
                 final String urlHost = uri.getHost();
 
                 final Matcher m = TLD_REGEX.matcher(urlHost);
-                isValidSalesforceHostname = (m.matches() && SALESFORCE_TLDS.contains(m.group(1)));
+                isValidSalesforceHostname = (m.matches() && VALID_SALESFORCE_DOMAINS.contains(m.group(1)));
 
-            } catch (URISyntaxException e) {
-                throw new IllegalArgumentException(endpointUrlParam + " is not a valid URL.");
-            } catch (UnsupportedEncodingException e) {
-                throw new IllegalArgumentException(endpointUrlParam + " is not a valid URL.");
+            } catch (Exception e) {
+                return baseAuthorizeURL;
             }
 
-            if (isValidSalesforceHostname) {
-                return super.buildAuthorizeUrl(grantType, parameters).replaceFirst(baseUrl, endpointUrl);
-            } else {
-                throw new IllegalArgumentException(endpointUrl + " is not a valid salesforce domain.");
-            }
-
+            return isValidSalesforceHostname ? baseAuthorizeURL.replaceFirst(baseUrl, endpointUrl) : baseAuthorizeURL;
         }
-        }
+    }
 
 
     @Override
